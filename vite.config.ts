eb395c11b2f5c3fb5b77e3171369720246cfa013
Phan Huu Phuoc <phuochuu0512@gmail.com>
2026-06-13 11:11:51 +0700

    init project

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  server: {
+    port: 3000, // Replace with your preferred port number
+  },
+});
